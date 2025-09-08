f31965100159568f8af9425e89a530c900812279
Krzysztof Wojtowicz <wojkrzysztof342@gmail.com>
2025-09-08 18:23:42 +0200

    main: add debug and test code

--- Src/Core/Src/main.c
+++ Src/Core/Src/main.c
@@ -18,12 +18,26 @@
 
 #include <stdint.h>
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
 
 int main(void)
 {
     /* Loop forever */
-	for(;;);
+    /* Debug and test code */
+    /* 
+    #include "../../hw/uc_hw/tim/tim.h"
+    #include "../../hw/uc_hw/core_init/core_init.h"
+    #include "../../hw/uc_hw/i2c/i2c.h"
+    #include "stm32c0xx.h"
+    core_init();
+    TIM3_init_pwm();
+    i2c1_init();
+    aa();
+    
+	  for(;;)
+    {
+      bb();
+    }
+    */  
 }
+
+
