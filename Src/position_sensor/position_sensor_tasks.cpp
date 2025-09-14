2df1f11d64e6429ab9218efe7c3367a67330a2da
Krzysztof Wojtowicz <wojkrzysztof342@gmail.com>
2025-09-14 19:10:13 +0200

    position_sensor: add calculating current position task function

---
+++ Src/position_sensor/position_sensor_tasks.cpp
@@ -0,0 +1,31 @@
+#include "position_sensor_tasks.hpp"
+#include "position_sensor.hpp"
+
+#define SENSOR_READ_SAMPLE_TIME_MS     5
+
+void calc_curr_pos(void *params)
+{
+    /* Initialisation */
+    position_sensor& sensor = *static_cast<position_sensor*>(params);
+    sensor.init(SENSOR_READ_SAMPLE_TIME_MS);
+    rpy_data data = {0};
+    pos_data_queue = rtos_queue_create(3, sizeof(struct rpy_data));
+    pos_calc_sem = rtos_sem_bin_create();
+
+    /* Data calculations */
+    while(1)
+    {
+        if (rtos_sem_take(pos_calc_sem, RTOS_MAX_DELAY) == 1)
+        {
+            sensor.update_data();
+            data.roll = sensor.get_roll();
+            data.pitch = sensor.get_pitch();
+            data.yaw = sensor.get_yaw();
+
+            /* Send current position to maintaining balance task */
+            rtos_queue_send(pos_data_queue, &data, RTOS_MAX_DELAY);
+        }
+    }
+
+    /* Task should never reach that point */
+}
